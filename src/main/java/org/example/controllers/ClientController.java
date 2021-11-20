d99af269913c9a51feb3026a0f3560742ca19e14
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-11-21 01:44:31 +0200

    Added basic code

---
+++ src/main/java/org/example/controllers/ClientController.java
@@ -0,0 +1,51 @@
+package org.example.controllers;
+
+import lombok.AllArgsConstructor;
+import org.example.dao.ClientRepository;
+import org.example.models.Client;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+@AllArgsConstructor
+public class ClientController {
+
+    private final ClientRepository clientRepository;
+
+    @GetMapping
+    public List<Client> getClients() {
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Client getClient(@PathVariable Long id) {
+        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createClient(@RequestBody Client client) throws URISyntaxException {
+        Client savedClient = clientRepository.save(client);
+        return ResponseEntity.created(new URI("/clients/" + savedClient.getId())).body(savedClient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateClient(@PathVariable Long id, @RequestBody Client client) {
+        Client currentClient = clientRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentClient.setName(client.getName());
+        currentClient.setEmail(client.getEmail());
+        currentClient = clientRepository.save(client);
+
+        return ResponseEntity.ok(currentClient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable Long id) {
+        clientRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/org/example/models/Client.java
@@ -0,0 +1,25 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
